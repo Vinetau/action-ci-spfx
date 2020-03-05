a093a19ff541b3c4d2858812b49113c7c896b0b5
Sebastian Brown <SBrown@forsythesit.com.au>
2020-03-05 14:58:27 +1100

    added readme, removed unused code

---
+++ README.md
@@ -0,0 +1,13 @@
+## Action CI Spfx
+
+This action builds and tests a spfx solution
+
+
+# Usage
+```
+    - uses: actions/checkout@v1    
+    - uses: actions/setup-node@v1
+      with:
+        node-version: '10.x'
+	- uses: forsythes-technology/action-ci-spfx@master
+```
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import * as artifact from "@actions/artifact";
 import * as core from "@actions/core";
 import { exec } from "@actions/exec";
 import * as github from "@actions/github";
@@ -15,29 +14,10 @@ async function main() {
 		await exec(`yarn test`);
 		core.info("(4/4) Package");
 		await exec(`yarn gulp package-solution --ship`);
-		// if (context.ref === "refs/heads/master") {
-		// 	// If no solution name is provided we assume that the solution filename is repo_name.sppkg
-		// 	solutionName = solutionName ? solutionName : `${repo}.sppkg`;
-		// 	createArtifact([`sharepoint\\solution\\${solutionName}`], repo);
-		// }
 		core.info(`✅ complete`);
 	} catch (err) {
 		core.error("❌ Failed");
 		core.setFailed(err.message);
-	}
-}
-
-async function createArtifact(files: string[], artifactName: string) {
-	core.info(`📦 Creating artifact ${artifactName}...`);
-	const artifactClient = artifact.create();
-	try {
-		const response = await artifactClient.uploadArtifact(artifactName, files, ".");
-		core.info(`Artifact ${response.artifactName} (size: ${response.size}) uploaded`);
-		core.info(`✅ complete`);
-	} catch (error) {
-		core.error("❌ Could not create artifact");
-		core.setFailed(error.message);
-
 	}
 }
 
